3a8ad29ff7373fd16b1c7aed7a383be93566ea21
Shadow-Dragon <Shadow-Dragon@uga-agga.de>
2011-09-13 01:55:02 +0200

    Bug fixes & Einige Seiten überarbeitet & Stamme Details Seite hinzugefügt

--- src/game/include/tribeDetail.html.php
+++ src/game/include/tribeDetail.html.php
@@ -13,26 +13,36 @@
 defined('_VALID_UA') or die('Direct Access to this location is not allowed.');
 
 function tribe_getContent($caveID, $tag) {
-  global $db, $no_resource_flag;
+  global $db, $template, $no_resource_flag;
 
   $no_resource_flag = 1;
-  
-  $sql = $db->prepare("SELECT t.*, p.playerID, p.name AS leaderName ".
-                      "FROM ". TRIBE_TABLE ." t ".
-                      "LEFT JOIN ". PLAYER_TABLE ." p ".
-                      "ON p.playerID = t.leaderID ".
-                      "WHERE t.tag LIKE :tag");
+
+  if (!$tag) {
+    $template->throwError('Es wurde kein Stamm ausgewählt.');
+    return;
+  }
+
+  // open template
+  $template->setFile('tribeDetail.tmpl');
+
+  $sql = $db->prepare("SELECT t.*, p.playerID, p.name AS leader_name
+                       FROM ". TRIBE_TABLE ." t
+                         LEFT JOIN ". PLAYER_TABLE ." p
+                           ON p.playerID = t.leaderID
+                       WHERE t.tag LIKE :tag");
   $sql->bindValue('tag', $tag, PDO::PARAM_STR);
-  if(!$sql->execute()) page_dberror();
+  if(!$sql->execute()) {
+    $template->throwError('Fehler in der Datenbank.');
+    return;
+  }
 
-
-  if (!$row = $sql->fetch(PDO::FETCH_ASSOC)) return "";
+  if (!$row = $sql->fetch(PDO::FETCH_ASSOC)) {
+    $template->throwError('Es konnte kein Stamm mit dem Namen gefunden werden.');
+    return;
+  }
   $sql->closeCursor();
 
-
   $JuniorAdmin = $targetPlayer = new Player(getPlayerByID($row['juniorLeaderID']));
-
-  $template = tmpl_open($_SESSION['player']->getTemplatePath() . 'tribeDetail.ihtml');
 
   $row["urltag"] = urlencode(unhtmlentities($tag));
   $row["playerList_modus"]   = TRIBE_PLAYER_LIST;
@@ -44,50 +54,52 @@ function tribe_getContent($caveID, $tag) {
   if (!empty($row['awards'])){
     $tmp = explode('|', $row['awards']);
     $awards = array();
-    foreach ($tmp AS $tag1) $awards[] = array('award_tag' => $tag1, 'award_modus' => AWARD_DETAIL);
+
+    foreach ($tmp AS $tag1) {
+      $awards[] = array(
+        'award_tag' => $tag1,
+        'award_modus' => AWARD_DETAIL
+      );
+    }
+
     $row['award'] = $awards;
   }
 
   if ($row['avatar']) {
-    $size = getimagesize($row['avatar']);  
-  
-    tmpl_set($template, 'AVATAR_IMG/avatar', $row['avatar']);
-    tmpl_set($template, 'AVATAR_IMG/width',  ($size[0] <= MAX_AVATAR_WIDTH) ? $size[0] : MAX_AVATAR_WIDTH);
-    tmpl_set($template, 'AVATAR_IMG/height', ($size[1] <= MAX_AVATAR_HEIGHT) ? $size[1] : MAX_AVATAR_HEIGHT);
+    $size = getimagesize($row['avatar']);
+    $row['avatar_width'] = ($size[0] <= MAX_AVATAR_WIDTH) ? $size[0] : MAX_AVATAR_WIDTH;
+    $row['avatar_heigt'] = ($size[1] <= MAX_AVATAR_HEIGHT) ? $size[1] : MAX_AVATAR_HEIGHT;
   }
 
-  tmpl_set($template, 'DETAILS/tag', $tag);
-  tmpl_set($template, 'DETAILS/name', $row['name']);
-
-  
-  foreach($row as $k => $v)
+  foreach($row as $k => $v) {
     if (!$v) $row[$k] = "k.A.";
+  }
 
-  $row['juniorLeaderName'] = $JuniorAdmin->name;
-  $row['juniorLeaderID'] = $JuniorAdmin->playerID;
+  $row['junior_leader_name'] = $JuniorAdmin->name;
+  $row['junior_leader_id'] = $JuniorAdmin->playerID;
   
-  $row['tribeRankingOffset'] = rankingTribe_checkOffset($tag);
+  $row['rank_offset'] = rankingTribe_checkOffset($tag);
 
   // parse tribe message
   $parser = new parser;
   $row['description'] = $parser->p($row['description']);
 
   // get Tribe Rank
-  $sql = $db->prepare("SELECT rank FROM ". RANKING_TRIBE_TABLE ." 
-                       WHERE tribe = :tag");
+  $sql = $db->prepare("SELECT rank FROM ". RANKING_TRIBE_TABLE ." WHERE tribe = :tag");
   $sql->bindValue('tag', $tag, PDO::PARAM_STR);
-  
-  if (!$sql->execute())
-    page_dberror();
 
-  if ($row2 = $sql->fetch(PDO::FETCH_ASSOC))
-    $row['rank'] = $row2['rank'];
-  else
+  if (!$sql->execute()) {
+    $template->throwError('Fehler in der Datenbank.');
+    return;
+  }
+
+  if ($ranking = $sql->fetch(PDO::FETCH_ASSOC)) {
+    $row['rank'] = $ranking['rank'];
+  } else {
     $row['rank'] = '';
+  }
 
-  tmpl_set($template, 'DETAILS', $row);
-
-  return tmpl_parse($template);
+  $template->addVar('tribe_details', $row);
 }
 
 ?>
