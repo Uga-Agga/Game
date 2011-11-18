2b0ce3a1dbafef0682aad11bdd939e2eed9f4b01
Shadow-Dragon <shadow-dragon@uga-agga.de>
2011-11-18 22:34:20 +0100

    Anzeige des Götterbildes im Ranking direkt aus der Datenbank auslesen. (Schutz vor manipulation/generieren unnötiger bilder)

--- src/www/images/religious_distribution/religious_distribution.php
+++ src/www/images/religious_distribution/religious_distribution.php
@@ -9,8 +9,30 @@
  * the License, or (at your option) any later version.
  */
 
-$uga_part   = intval($_GET['ugapercent']);
-$agga_part  = intval($_GET['aggapercent']);
+/** Set flag that this is a parent file */
+define("_VALID_UA", 1);
+
+require_once("../../config.inc.php");
+require_once("include/config.inc.php");
+require_once("include/params.inc.php");
+require_once("include/db.inc.php");
+require_once("include/ranking.inc.php");
+
+$config = new Config();
+$db     = DbConnect();
+
+$religions =  ranking_getReligiousDistribution();
+
+$ugaCount = (isset($religions['uga'])) ? $religions['uga'] : 0;
+$aggaCount = (isset($religions['agga'])) ? $religions['agga'] : 0;
+
+if (($ugaCount + $aggaCount) != 0) {
+  $uga_part = round($ugaCount/($ugaCount + $aggaCount)*100);
+  $agga_part = round($aggaCount/($ugaCount + $aggaCount)*100);
+} else {
+  $uga_part = 0;
+  $agga_part = 0;
+}
 
 $filename = reldis_getFilename($uga_part, $agga_part);
 
@@ -21,24 +43,18 @@ header("Location: $filename");
 
 
 ################################################################################
-
-
 function reldis_getFilename($uga_part, $agga_part){
-  return sprintf('../temp/reldis_%d_%d.png',
-                 $uga_part, $agga_part);
+  return sprintf('../temp/reldis_%d_%d.png', $uga_part, $agga_part);
 }
 
-
 ################################################################################
-
-
-function reldis_createFile($filename, $uga_part, $agga_part){
-
+function reldis_createFile($filename, $uga_part, $agga_part) {
   $im_uga   = @ImageCreateFromPng("good.png");
   $im_agga  = @ImageCreateFromPng("bad.png");
 
-  if (!$im_uga || !$im_agga)
+  if (!$im_uga || !$im_agga) {
     die ("Cannot Initialize new GD image stream");
+  }
 
   $width      = imagesx($im_uga);
 
