cbf2e2e73320f12902269d595db2e438dece5fe1
David <unger.dave@googlemail.com>
2013-02-26 00:30:57 +0100

    Update Copyright

--- src/game/include/playerDetail.html.php
+++ src/game/include/playerDetail.html.php
@@ -2,7 +2,7 @@
 /*
  * playerDetail.html.php -
  * Copyright (c) 2003  OGP Team
- * Copyright (c) 2011-2012 David Unger <unger-dave@gmail.com>
+ * Copyright (c) 2011-2013 David Unger <unger-dave@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
