a90c37d0c11b2cf28e772bc0edc63dd5ca1f7463
David <unger.dave@googlemail.com>
2013-03-04 23:04:05 +0100

    Fix Tribe

--- src/utilities/createDailyPlayerSheet.php
+++ src/utilities/createDailyPlayerSheet.php
@@ -2,6 +2,7 @@
 /*
  * createDailyPlayerSheet.php -
  * Copyright (c) 2004  OGP Team
+ * Copyright (c) 2013 David Unger <unger-dave@gmail.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -18,12 +19,13 @@ $db     = DbConnect();
 
 $sql = $db->prepare("SELECT CONCAT(\"'\", p.name, \"'\"),
                        r.rank, r.average,
-                       CONCAT(\"'\", p.tribe, \"'\"),
+                       CONCAT(\"'\", t.tag, \"'\"),
                        r.religion,
                        count(*) as anzahl
                      FROM " . CAVE_TABLE . " c
                        RIGHT JOIN " . PLAYER_TABLE . " p ON p.playerID = c.playerID
                        LEFT JOIN " . RANKING_TABLE . " r ON r.playerID = p.playerID
+                       LEFT JOIN " . TRIBE_TABLE . " t ON t.tribeID = p.tribeID
                      GROUP BY p.playerID");
 if (!$sql->execute()) {
   die("Fehler beim Auslesen.");
