30e5a9b6916e2b5e29ccbcd1962a792eb43d3269
David <david@edv-unger.com>
2016-03-30 11:14:38 +0200

    IE11 chat fix

--- src/www/js/jappixmini.js
+++ src/www/js/jappixmini.js
@@ -1872,7 +1872,7 @@ JSJaCPacket.prototype.setXMLLang = function(xmllang) {
 
   // Also due to issues with both BD and jQuery being used, employ a simple regexp since the detection
   // here is very limited.
-  if (navigator.appVersion.match(/^.*MSIE (\d)/) && navigator.appVersion.match(/^.*MSIE (\d)/)[1] >= 9)
+  if (navigator.appVersion.match(/^.*MSIE (\d)/) || navigator.userAgent.match(/Trident\/(\d+)((\.)(\d+))?/))
     return this;
   if (!xmllang || xmllang == '')
     this.getNode().removeAttribute('xml:lang');
